bc8928878adcce4532224e5ed392d3804c80d3fb
Edward <edward.b.workin@gmail.com>
2022-10-30 18:16:31 -0400

    finished js regex etc
    finished regex and wrong input prevention and the animation to show the data on the cards. started on error messages for form submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //! Selectors
 
-// const chname = document.getElementById('chname');
+const chname = document.getElementById('chname');
 const cnum = document.getElementById('cnum');
 const expmm = document.getElementById('expmm');
 const expyy = document.getElementById('expyy');
@@ -8,7 +8,20 @@ const cvc = document.getElementById('cvc');
 // const submitBtn = document.getElementById('submit-btn');
 const form = document.getElementById('form');
 
+const bgNum = document.getElementById('card-front-num');
+const bgName = document.getElementById('card-front-name');
+const bgMm = document.getElementById('card-front-exp-mm');
+const bgYy = document.getElementById('card-front-exp-yy');
+const bgCvc = document.getElementById('card-back-cvc');
+
+const errorCnum = document.getElementById('cnum-error');
+const errorExp = document.getElementById('exp-error');
+const errorCvc = document.getElementById('cvc-error');
+
+
 //! Listeners
+
+// stop keydown of anything but digits and backspace
 
 const qual = /[0-9]/ ;
 
@@ -40,24 +53,82 @@ cvc.addEventListener("keydown", (e) => {
     }
 })
 
+// edit background cards when there is a new input value
+
+chname.addEventListener("keyup", (e) => {
+    if (chname.value !== ""){
+        chnameTextEdit()
+    }
+    if (chname.value == ""){
+        bgName.innerText = "Jane Appleseed"
+    }
+})
+
+cnum.addEventListener("keyup", (e) => {
+    if (cnum.value !== ""){
+        cnumTextEdit()
+    }
+    if (cnum.value == ""){
+        bgNum.innerText = "0000 0000 0000 0000"
+    }
+})
+
 expmm.addEventListener("keyup", (e) => {
-    let key = e.key;
+    if (expmm.value !== ""){
+        expmmTextEdit()
+    }
+    if (expmm.value == ""){
+        bgMm.innerText = "00"
+    }
+    
+    
     if (expmm.value == "00"){
         expmm.value = "01"
     }
 })
 
-cvc.addEventListener("keyup", (e) => {
-    let key = e.key;
-    cardTextEdit()
+expyy.addEventListener("keyup", (e) => {
+    if (expyy.value !== ""){
+        expyyTextEdit()
+    }
+    if (expyy.value == ""){
+        bgYy.innerText = "00"
+    }
 })
 
+cvc.addEventListener("keyup", (e) => {
+    if (cvc.value !== ""){
+        cvcTextEdit()
+    }
+    if (cvc.value == ""){
+        bgCvc.innerText = "000"
+    }
+})
+
+// stop form submitting unless all criteria is met once all of it is met send to next page
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault()
 })
 
 //! Functions
 
-function cardTextEdit () {
-    console.log(this.value)
+function chnameTextEdit () {
+    bgName.innerText = chname.value
+}
+
+function cnumTextEdit () {
+    bgNum.innerText = cnum.value
+}
+
+function expmmTextEdit () {
+    bgMm.innerText = expmm.value
+}
+
+function expyyTextEdit () {
+    bgYy.innerText = expyy.value
+}
+
+function cvcTextEdit () {
+    bgCvc.innerText = cvc.value
 }
